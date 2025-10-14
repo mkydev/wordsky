173816ad5210ad51449441aaa1d48e3706dffec3
KY <ky@KY-MacBook-Pro.local>
2025-10-15 00:26:14 +0300

    sng

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,7 +10,7 @@ import { io } from 'socket.io-client';
 const showSplash = ref(true);
 
 // --- Temel Değişkenler ---
-const apiUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const apiUrl = import.meta.env.VITE_API_BASE_URL || '';
 const socket = io(apiUrl);
 
 const apiWords = ref<string[]>([]);
