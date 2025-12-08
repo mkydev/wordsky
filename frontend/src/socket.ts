7df11423242b52261cf10fa4e05cee17e6c30153
KY <ky@KY-MacBook-Pro.local>
2025-12-08 18:10:55 +0300

    kl

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -6,7 +6,7 @@ export const state = reactive({
 });
 
 // Geliştirme ortamı için localhost, canlı ortam için sunucu adresinizi kullanın.
-const URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const URL = import.meta.env.VITE_API_URL;
 
 export const socket = io(URL, {
   autoConnect: false,
